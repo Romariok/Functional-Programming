e4afa60722327c827ee6ee37041a2d24a6863d63
Romariok <ronya-ko@yandex.ru>
2024-09-16 22:59:05 +0300

    fix linter warnings

--- apps/lab1/lib/prob6/prob6.ex
+++ apps/lab1/lib/prob6/prob6.ex
@@ -6,8 +6,7 @@ defmodule Prob6 do
   @doc """
   Monolitchic implementation with tail recursion
   """
-  @spec tail_recursion() :: number()
-  def tail_recursion() do
+  def tail_recursion do
     [sum, sum_of_sq] = sum_n_sq_tail_rec(100)
     :math.pow(sum, 2) - sum_of_sq
   end
@@ -22,8 +21,7 @@ defmodule Prob6 do
   @doc """
   Monolitchic implementation with recursion
   """
-  @spec recursion() :: number()
-  def recursion() do
+  def recursion do
     :math.pow(Enum.sum(1..100), 2) - sum_of_sq_rec(100)
   end
 
@@ -34,7 +32,7 @@ defmodule Prob6 do
   @doc """
   Modular implementation with sequence generation and reduce
   """
-  def modular() do
+  def modular do
     sq_of_sum =
       sequence_generator(100)
       |> Enum.reduce(0, &(&1 + &2))
@@ -51,7 +49,7 @@ defmodule Prob6 do
   @doc """
   Sequence generation using map
   """
-  def seq_gen_map() do
+  def seq_gen_map  do
     sq_of_sum =
       sequence_generator(100)
       |> Enum.sum()
@@ -67,7 +65,7 @@ defmodule Prob6 do
   @doc """
   Infinite structures and Lazy
   """
-  def inf_struct_lazy() do
+  def inf_struct_lazy do
     sq_of_sum =
       Stream.iterate(1, &(&1 + 1))
       |> Stream.take(100)
