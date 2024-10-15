dbb80a6319d679f2a4f83f04e8567cb7102994f1
Romariok <ronya-ko@yandex.ru>
2024-10-15 21:15:53 +0300

    maybe lab2

--- apps/lab2/lib/AVLDict.ex
+++ apps/lab2/lib/AVLDict.ex
@@ -6,14 +6,10 @@ defmodule AVLDict do
     @moduledoc """
     Node of the AVL Tree
     """
-    defstruct [:key, :value, :left, :right, :height]
+    defstruct [:key, :value, :height, :left, :right]
 
     def new do
       %Node{key: nil, value: nil, left: nil, right: nil, height: 0}
-    end
-
-    def height(%Node{height: height}) do
-      height
     end
   end
 
@@ -21,88 +17,375 @@ defmodule AVLDict do
     @moduledoc """
     Dictionary that implement an AVL Tree
     """
-    def left_rotate(%Node{right: ptr1} = t) do
-      ptr2 = ptr1.left
-      ptr1 = %{ptr1 | left: %{t | right: ptr2, height: t.height - 1}}
-      ptr1
-    end
+    @empty_node %Node{key: nil, value: nil, left: nil, right: nil, height: 0}
 
-    def right_rotate(%Node{left: ptr1} = t) do
-      ptr2 = ptr1.right
-      ptr1 = %{ptr1 | right: %{t | left: ptr2, height: t.height - 1}}
-      ptr1
-    end
-
-    def height(nil), do: 0
+    def height(@empty_node), do: 0
 
     def height(%Node{left: left, right: right}) do
       max(height(left), height(right)) + 1
     end
 
-    def balance(%Node{left: %Node{height: left_height}, right: %Node{height: right_height}} = node)
+    def balance(
+          %Node{left: %Node{height: left_height}, right: %Node{height: right_height}} = node
+        )
         when abs(left_height - right_height) != 2 do
       node
     end
 
     # Right rotation
-    def balance(%Node{left: %Node{height: left_height}, right: %Node{height: right_height}} = node)
-        when left_height == right_height + 1 do
-      %Node{right_rotate(node) | height: height(right_rotate(node))}
+    def balance(
+          %Node{
+            left:
+              %Node{
+                left: %Node{height: left_left_height} = left_left,
+                right: %Node{height: left_right_height} = left_right
+              } = left,
+            right: %Node{height: right_height}
+          } = node
+        )
+        when left_left_height == right_height + 1 do
+      new_height_right = max(right_height, left_right_height) + 1
+
+      %Node{
+        left
+        | height: max(new_height_right, left_right_height) + 1,
+          right: %Node{node | height: new_height_right, left: left_right},
+          left: left_left
+      }
     end
 
     # Left rotation
-    def balance(%Node{left: %Node{height: left_height}, right: %Node{height: right_height}} = node)
-        when left_height + 1 == right_height do
-      %Node{left_rotate(node) | height: height(left_rotate(node))}
-    end
-
-    # Big left rotation
     def balance(
           %Node{
-            right: %Node{left: %Node{height: right_left_height}},
-            left: %Node{height: left_height}
+            left: %Node{height: left_height},
+            right:
+              %Node{
+                right: %Node{height: right_right_height} = right_right,
+                left: %Node{height: right_left_height} =  right_left
+              } = right
           } = node
         )
-        when right_left_height == left_height + 1 do
-      new_node = %Node{
-        node
-        | right: %Node{right_rotate(node.right) | height: height(right_rotate(node.right))}
-      }
+        when left_height + 1 == right_right_height do
+      new_height_left = max(left_height, right_left_height) + 1
 
-      new_node = %Node{left_rotate(new_node) | height: height(left_rotate(new_node))}
-      %Node{new_node | left: %Node{new_node.left | height: height(new_node.left)}}
+      %Node{
+        right
+        | height: max(new_height_left, right_left_height) + 1,
+          left: %Node{node | height: new_height_left, right: right_left},
+          right: right_right
+      }
     end
 
     # Big right rotation
     def balance(
           %Node{
-            left: %Node{right: %Node{height: left_right_height}},
+            left:
+              %Node{
+                right:
+                  %Node{height: left_right_height, left: left_right_left, right: left_right_right} =
+                    left_right,
+                left: %Node{height: left_left_height} = left_left
+              } = left,
             right: %Node{height: right_height}
           } = node
         )
         when left_right_height == right_height + 1 do
-      new_node = %Node{
-        node
-        | left: %Node{left_rotate(node.left) | height: height(left_rotate(node.left))}
+      %Node{
+        left_right
+        | height: left_left_height + 2,
+          left: %Node{
+            left
+            | height: left_left_height + 1,
+              left: left_left,
+              right: left_right_left
+          },
+          right: %Node{node | height: left_left_height + 1, left: left_right_right}
       }
-
-      new_node = %Node{right_rotate(new_node) | height: height(right_rotate(new_node))}
-      %Node{new_node | right: %Node{new_node.right | height: height(new_node.right)}}
     end
 
-    def insert(nil, _key, _value), do: Node.new()
+    # Big left rotation
+    def balance(
+          %Node{
+            right:
+              %Node{
+                left:
+                  %Node{height: right_left_height, left: right_left_left, right: right_left_right} =
+                    right_left,
+                right: %Node{height: right_right_height} = right_right
+              } = right,
+            left: %Node{height: left_height}
+          } = node
+        )
+        when right_left_height == left_height + 1 do
+      %Node{
+        right_left
+        | height: right_right_height + 2,
+          left: %Node{node | height: right_right_height + 1, right: right_left_left},
+          right: %Node{
+            right
+            | height: right_right_height + 1,
+              left: right_left_right,
+              right: right_right
+          }
+      }
+    end
+
+    def insert(@empty_node, key, value),
+      do: %Node{key: key, value: value, height: 1, left: @empty_node, right: @empty_node}
 
     def insert(%Node{key: node_key, value: _node_value} = node, key, value) when node_key == key,
       do: %Node{node | value: value}
 
-    def insert(%Node{key: node_key, left: left} = node, key, value) when node_key > key do
+    def insert(%Node{key: node_key, left: left, height: node_height} = node, key, value)
+        when node_key > key do
       new_small = insert(left, key, value)
-      balance(%Node{node | left: new_small, height: max(height(new_small) + 1, node.height)})
+      balance(%Node{node | left: new_small, height: max(height(new_small) + 1, node_height)})
     end
 
-    def insert(%Node{key: node_key, right: right} = node, key, value) when node_key < key do
+    def insert(%Node{key: node_key, right: right, height: node_height} = node, key, value)
+        when node_key < key do
       new_big = insert(right, key, value)
-      balance(%Node{node | right: new_big, height: max(height(new_big) + 1, node.height)})
+      balance(%Node{node | right: new_big, height: max(height(new_big) + 1, node_height)})
+    end
+
+    def find(_key, @empty_node), do: :not_found
+    def find(key, %Node{key: key, value: value}), do: {key, value}
+    def find(key, %Node{key: node_key, left: left}) when node_key > key, do: find(key, left)
+    def find(key, %Node{key: node_key, right: right}) when node_key < key, do: find(key, right)
+
+    def remove_min(%Node{key: key, value: value, height: _, left: @empty_node, right: right}) do
+      {%Node{key: key, value: value, height: nil, left: nil, right: nil}, right, true}
+    end
+
+    def remove_min(%Node{key: key, value: value, height: _, left: left, right: @empty_node}) do
+      {left, %Node{key: key, value: value, height: 1, left: @empty_node, right: @empty_node},
+       true}
+    end
+
+    def remove_min(%Node{
+          key: key,
+          value: value,
+          height: _,
+          left: left,
+          right:
+            %Node{
+              key: key_r,
+              value: value_r,
+              height: height_r,
+              left: %Node{height: height_left_r} = left_r,
+              right: %Node{height: height_right_r} = right_r
+            } = right
+        }) do
+      {min, new_left, is_last_call} = remove_min(left)
+
+      case {is_last_call, %Node{height: height_new_left} = new_left, right_r, height_r} do
+        {true, @empty_node, @empty_node, 2} ->
+          {min,
+           %Node{
+             left_r
+             | height: 2,
+               left: %Node{
+                 key: key,
+                 value: value,
+                 height: 1,
+                 left: @empty_node,
+                 right: @empty_node
+               },
+               right: %Node{
+                 key: key_r,
+                 value: value_r,
+                 height: 1,
+                 left: @empty_node,
+                 right: @empty_node
+               }
+           }, false}
+
+        {true, @empty_node, _, _} ->
+          {min,
+           %Node{
+             key: key_r,
+             value: value_r,
+             height: max(height_left_r + 1, height_right_r) + 1,
+             left: %Node{
+               key: key,
+               value: value,
+               left: @empty_node,
+               right: left_r,
+               height: height_left_r + 1
+             },
+             right: right_r
+           }, false}
+
+        _ ->
+          {min,
+           balance(%Node{
+             key: key,
+             value: value,
+             height: max(height_new_left + 1, height_r) + 1,
+             left: new_left,
+             right: right
+           }), false}
+      end
+    end
+
+    def do_remove(_key, @empty_node), do: :not_found
+
+    def do_remove(key, %Node{
+          key: key,
+          value: _value,
+          height: _,
+          left: @empty_node,
+          right: @empty_node
+        }),
+        do: @empty_node
+
+    def do_remove(key, %Node{key: key, value: _value, height: _, left: left, right: @empty_node}),
+      do: left
+
+    def do_remove(key, %Node{key: key, value: _value, height: _, left: @empty_node, right: right}),
+      do: right
+
+    def do_remove(key, %Node{
+          key: key,
+          value: _value,
+          height: _,
+          left: %Node{height: height_left} = left,
+          right: right
+        }) do
+      {min, %Node{height: height_new_right} = new_right, _} = remove_min(right)
+
+      case min do
+        @empty_node ->
+          @empty_node
+
+        %Node{key: min_key, value: min_value} ->
+          balance(%Node{
+            key: min_key,
+            value: min_value,
+            left: left,
+            right: new_right,
+            height: max(height_left, height_new_right) + 1
+          })
+      end
+    end
+
+    def do_remove(target_key, %Node{
+          key: key,
+          value: value,
+          height: _h,
+          left: left,
+          right: %Node{height: height_right} = right
+        })
+        when target_key < key do
+      new_left = do_remove(target_key, left)
+      case {new_left, right} do
+        {:not_found, _} ->
+          :not_found
+
+        {@empty_node, @empty_node} ->
+          %Node{key: key, value: value, height: 1, left: @empty_node, right: @empty_node}
+
+        {%Node{height: height_new_left}, _} ->
+          balance(%Node{
+            key: key,
+            value: value,
+            height: max(height_new_left, height_right) + 1,
+            left: new_left,
+            right: right
+          })
+      end
+    end
+
+    def do_remove(target_key, %Node{
+          key: key,
+          value: value,
+          height: _h,
+          left: %Node{height: height_left} = left,
+          right: right
+        })
+        when target_key > key do
+      new_right = do_remove(target_key, right)
+      case {left, new_right} do
+        {_, :not_found}->
+          :not_found
+
+        {@empty_node, @empty_node} ->
+          %Node{key: key, value: value, height: 1, left: @empty_node, right: @empty_node}
+
+        {_, %Node{height: height_new_right}} ->
+          balance(%Node{
+            key: key,
+            value: value,
+            height: max(height_new_right, height_left) + 1,
+            left: left,
+            right: new_right
+          })
+      end
+    end
+
+    def remove(key, tree) do
+      case do_remove(key, tree) do
+        :not_found -> tree
+        {_, new_tree, _} -> new_tree
+        new_tree -> new_tree
+      end
+    end
+
+    def to_list(@empty_node), do: []
+
+    def to_list(%Node{key: key, value: value, left: left, right: right}) do
+      to_list(left) ++ [{key, value}] ++ to_list(right)
+    end
+
+    def from_list(list) do
+      :lists.foldl(fn {key, value}, acc -> insert(acc, key, value) end, @empty_node, list)
+    end
+
+    def map(nil, _), do: []
+
+    def map(%Node{key: key, value: value, left: left, right: right}, fun) do
+      map(left, fun) ++ [fun.({key, value})] ++ map(right, fun)
+    end
+
+    def map_tree(node, fun) do
+      from_list(map(node, fun))
+    end
+
+    def foldl(@empty_node, acc, _), do: acc
+
+    def foldl(%Node{key: key, value: value, left: left, right: right}, acc, fun) do
+      foldl(right, fun.(foldl(left, acc, fun), {key, value}), fun)
+    end
+
+    def foldr(@empty_node, acc, _), do: acc
+
+    def foldr(%Node{key: key, value: value, left: left, right: right}, acc, fun) do
+      foldr(left, fun.(foldr(right, acc, fun), {key, value}), fun)
+    end
+
+    def filter(@empty_node, _), do: []
+
+    def filter(%Node{key: key, value: value, height: _, left: left, right: right}, func) do
+      case func.(key, value) do
+        true -> filter(left, func) ++ [key, value] ++ filter(right, func)
+        false -> filter(left, func) ++ filter(right, func)
+      end
+    end
+
+    def filter_tree(tree, func), do: from_list(filter(tree, func))
+
+    def merge(x, y) do
+      foldl(y, x, fn acc, {key, value} -> insert(acc, key, value) end)
+    end
+
+    def equal_tree(x, y) do
+      lenx = foldl(x, 0, fn acc, _ -> acc + 1 end)
+      leny = foldl(y, 0, fn acc, _ -> acc + 1 end)
+
+      case lenx == leny do
+        false -> false
+        _ -> foldl(y, true, fn acc, {key, value} -> acc and find(key, x) === {key, value} end)
+      end
     end
   end
 end
--- apps/lab2/test/lab2_test.exs
+++ apps/lab2/test/lab2_test.exs
@@ -4,182 +4,196 @@ defmodule Lab2Test do
   use ExUnit.Case
   doctest AVLDict
 
-  test "Height of node with no child" do
-    node_height = Dictionary.height(Node.new())
-    assert node_height == 1
+  @empty_node %Node{key: nil, value: nil, left: nil, right: nil, height: 0}
+
+  def balance_check(%Node{key: nil, value: nil, height: height, left: nil, right: nil}),
+    do: height == 0
+
+  def balance_check(%Node{
+        key: _key,
+        value: _value,
+        height: height,
+        left: %Node{height: left_height} = left,
+        right: %Node{height: right_height} = right
+      }) do
+    balance_check(left) and balance_check(right) and height == max(left_height, right_height) + 1 and
+      right_height - left_height < 2 and -2 < right_height - left_height
   end
 
-  test "Height of node with one child" do
-    node_height = Dictionary.height(%Node{right: Node.new()})
-    assert node_height == 2
+  def insert_all(l) do
+    r =
+      :lists.foldl(
+        fn {key, value}, acc ->
+          cur = Dictionary.insert(acc, key, value)
+          assert balance_check(cur) == true
+        end,
+        @empty_node,
+        l
+      )
+
+    list_r = Dictionary.to_list(r)
+    assert ^list_r = :lists.usort(l)
   end
 
-  test "Height of node with two childs with heights 2 and 3" do
-    node_height =
-      Dictionary.height(%Node{
-        Node.new()
-        | right: %Node{left: Node.new()},
-          left: %Node{left: %Node{right: Node.new()}}
-      })
+  # Добавление
 
-    assert node_height == 4
-  end
-
-  test "Left rotation" do
-    node = %Node{
-      key: 1,
-      value: "value1",
-      left: %Node{key: 2, value: "value2", height: 1},
-      right: %Node{
-        key: 3,
-        value: "value3",
-        left: %Node{key: 5, value: "value5", height: 1},
-        right: %Node{key: 4, value: "value4", height: 1},
-        height: 2
-      },
-      height: 3
-    }
-
-    expected_node = %Node{
-      key: 3,
-      value: "value3",
-      left: %Node{
-        key: 1,
-        value: "value1",
-        left: %Node{key: 2, value: "value2", height: 1},
-        right: %Node{key: 5, value: "value5", height: 1},
-        height: 2
-      },
-      right: %Node{key: 4, value: "value4", height: 1},
-      height: 3
-    }
-
-    assert %Node{
-             Dictionary.left_rotate(node)
-             | height: Dictionary.height(Dictionary.left_rotate(node))
-           } == expected_node
-  end
-
-  test "Right rotation" do
-    node = %Node{
-      key: 3,
-      value: "value3",
-      left: %Node{
-        key: 1,
-        value: "value1",
-        left: %Node{key: 2, value: "value2", height: 1},
-        right: %Node{key: 5, value: "value5", height: 1},
-        height: 2
-      },
-      right: %Node{key: 4, value: "value4", height: 1},
-      height: 3
-    }
+  test "One Node" do
+    node = Dictionary.insert(@empty_node, 1, "value1")
 
     expected_node = %Node{
       key: 1,
       value: "value1",
-      left: %Node{key: 2, value: "value2", height: 1},
-      right: %Node{
-        key: 3,
-        value: "value3",
-        left: %Node{key: 5, value: "value5", height: 1},
-        right: %Node{key: 4, value: "value4", height: 1},
-        height: 2
-      },
-      height: 3
+      height: 1,
+      left: @empty_node,
+      right: @empty_node
     }
 
-    assert %Node{
-             Dictionary.right_rotate(node)
-             | height: Dictionary.height(Dictionary.right_rotate(node))
-           } == expected_node
+    assert node == expected_node and Dictionary.find(0, node) == :not_found and
+             Dictionary.find(1, node) == {1, "value1"}
   end
 
-  test "Big right rotation" do
-    node = %Node{
-      key: 1,
-      value: "value1",
-      right: %Node{key: 2, value: "value2", height: 1},
-      left: %Node{
-        key: 3,
-        value: "value3",
-        left: %Node{key: 4, value: "value4", height: 1},
-        right: %Node{
-          key: 5,
-          value: "value5",
-          height: 2,
-          left: %Node{key: 6, value: "value6", height: 1},
-          right: %Node{key: 7, value: "value7", height: 1}
-        },
-        height: 3
-      },
-      height: 4
-    }
+  test "Two Node" do
+    node1 = Dictionary.insert(@empty_node, 1, "value1")
+    node2 = Dictionary.insert(node1, 2, "value2")
 
     expected_node = %Node{
-      key: 5,
-      value: "value5",
-      left: %Node{
-        key: 3,
-        value: "value3",
-        left: %Node{key: 4, value: "value4", height: 1},
-        right: %Node{key: 6, value: "value6", height: 1},
-        height: 2
-      },
-      right: %Node{
-        key: 1,
-        value: "value1",
-        left: %Node{key: 7, value: "value7", height: 1},
-        right: %Node{key: 2, value: "value2", height: 1},
-        height: 2
-      },
-      height: 3
+      key: 1,
+      value: "value1",
+      height: 2,
+      left: @empty_node,
+      right: %Node{key: 2, value: "value2", height: 1, left: @empty_node, right: @empty_node}
     }
 
-    assert Dictionary.balance(node) == expected_node
+    assert node2 == expected_node
   end
 
-  test "Big left rotation" do
-    node = %Node{
-      key: 1,
-      value: "value1",
-      left: %Node{key: 2, value: "value2", height: 1},
-      right: %Node{
-        key: 3,
-        value: "value3",
-        right: %Node{key: 4, value: "value4", height: 1},
-        left: %Node{
-          key: 5,
-          value: "value5",
-          height: 2,
-          left: %Node{key: 6, value: "value6", height: 1},
-          right: %Node{key: 7, value: "value7", height: 1}
-        },
-        height: 3
-      },
-      height: 4
-    }
+  test "Six Node" do
+    l = [{1, "value1"}, {2, "value2"}, {3, "value3"}, {4, "value4"}, {5, "value5"}, {6, "value6"}]
 
     expected_node = %Node{
-      key: 5,
-      value: "value5",
-      right: %Node{
-        key: 3,
-        value: "value3",
-        right: %Node{key: 4, value: "value4", height: 1},
-        left: %Node{key: 7, value: "value7", height: 1},
-        height: 2
-      },
+      key: 4,
+      value: "value4",
+      height: 3,
       left: %Node{
-        key: 1,
-        value: "value1",
-        right: %Node{key: 6, value: "value6", height: 1},
-        left: %Node{key: 2, value: "value2", height: 1},
-        height: 2
+        key: 2,
+        value: "value2",
+        height: 2,
+        left: %Node{key: 1, value: "value1", height: 1, left: @empty_node, right: @empty_node},
+        right: %Node{key: 3, value: "value3", height: 1, left: @empty_node, right: @empty_node}
       },
-      height: 3
+      right: %Node{
+        key: 5,
+        value: "value5",
+        height: 2,
+        left: @empty_node,
+        right: %Node{key: 6, value: "value6", height: 1, left: @empty_node, right: @empty_node}
+      }
     }
 
-    assert Dictionary.balance(node) == expected_node
+    assert Dictionary.equal_tree(Dictionary.from_list(l), expected_node) == true
+  end
+
+  test "Big Node" do
+    l = Enum.map(1..10_000, fn x -> {x, "value#{x}"} end)
+    t = Dictionary.from_list(l)
+    balance_check(t)
+    assert Dictionary.to_list(t) == :lists.usort(l)
+  end
+
+  # Удаление
+
+  test "One Node Remove" do
+    t = Dictionary.from_list([{1, "value1"}])
+    assert Dictionary.remove(1, t) == @empty_node
+  end
+
+  test "One From Two Node Remove" do
+    t = Dictionary.from_list([{1, "value1"}, {2, "value2"}])
+    assert Dictionary.to_list(Dictionary.remove(1, t)) == [{2, "value2"}]
+  end
+
+  test "Two Node Remove" do
+    t = Dictionary.from_list([{1, "value1"}, {2, "value2"}, {3, "value3"}])
+
+    assert Dictionary.to_list(Dictionary.remove(2, t)) == [{1, "value1"}, {3, "value3"}] and
+             Dictionary.to_list(Dictionary.remove(1, t)) == [{2, "value2"}, {3, "value3"}]
+  end
+
+  test "Four Node Remove" do
+    t = Dictionary.from_list([{1, "value1"}, {0, "value0"}, {3, "value3"}, {4, "value4"}])
+
+    assert Dictionary.to_list(Dictionary.remove(3, t)) == [
+             {0, "value0"},
+             {1, "value1"},
+             {4, "value4"}
+           ]
+  end
+
+  test "Not Found Remove" do
+    t = Dictionary.from_list([{1, "value1"}, {2, "value2"}, {3, "value3"}])
+    assert Dictionary.remove(52, t) == t
+  end
+
+  def remove_all(l, t) do
+    {empty_t, _} =
+      :lists.foldl(
+        fn {k, _v}, {t_acc, l_acc} ->
+          newt = Dictionary.remove(k, t_acc)
+          [_ | newl] = l_acc
+          assert balance_check(newt) == true
+          assert :lists.usort(newl) == Dictionary.to_list(newt)
+          {newt, newl}
+        end,
+        {t, l},
+        l
+      )
+
+    assert empty_t == @empty_node
+  end
+
+  test "Remove All" do
+    l = [{1, "value1"}, {2, "value2"}, {3, "value3"}, {0, "value0"}]
+    t = Dictionary.from_list(l)
+    remove_all(l, t)
+  end
+
+  # Проверка свойства моноида
+  # Нейтральный элемент
+
+  def monoid_neutral_elem(t_size) do
+    t = Dictionary.from_list(Enum.map(1..t_size, fn _ -> {:rand.uniform(30), 0} end))
+    r = Dictionary.merge(t, @empty_node)
+    assert Dictionary.to_list(t) == Dictionary.to_list(r)
+    r2 = Dictionary.merge(@empty_node, t)
+    assert Dictionary.to_list(t) == Dictionary.to_list(r2)
+  end
+
+  test "Monoid Neutral Element" do
+    Enum.map(1..10_000, fn _ -> monoid_neutral_elem(:rand.uniform(1000) - 1) end)
+  end
+
+  # Ассоциативность операции умножения - слияние деревьев
+
+  def associativity(t1_size, t2_size, t3_size) do
+    t1 = Dictionary.from_list(Enum.map(1..t1_size, fn _ -> {:rand.uniform(30), :rand.uniform(100)} end))
+    t2 = Dictionary.from_list(Enum.map(1..t2_size, fn _ -> {:rand.uniform(30), :rand.uniform(100)} end))
+    t3 = Dictionary.from_list(Enum.map(1..t3_size, fn _ -> {:rand.uniform(30), :rand.uniform(100)} end))
+
+    r1 = Dictionary.merge(t1, Dictionary.merge(t2, t3))
+    r2 = Dictionary.merge(Dictionary.merge(t1, t2), t3)
+
+    assert Dictionary.to_list(r1) == Dictionary.to_list(r2)
+  end
+
+  test "Small Monoid Test" do
+    Enum.map(1..10_000, fn _ -> associativity(:rand.uniform(10) - 1, :rand.uniform(10) - 1, :rand.uniform(10) - 1) end)
+  end
+
+  test "Medium Monoid Test" do
+    Enum.map(1..10_000, fn _ -> associativity(:rand.uniform(100) - 1, :rand.uniform(100) - 1, :rand.uniform(100) - 1) end)
+  end
+
+  test "Big Monoid Test" do
+    Enum.map(1..10_000, fn _ -> associativity(:rand.uniform(1000) - 1, :rand.uniform(1000) - 1, :rand.uniform(1000) - 1) end)
   end
 end
